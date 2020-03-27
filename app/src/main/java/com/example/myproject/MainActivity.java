e57b0fcf86f789366bb1bb0c139c6feea487e324
margleb <margleb@yandex.ru>
2020-03-27 14:52:28 +0300

    Building our First App + Finishing Our First App

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    private Button myButton;
+    private TextView showtext;
+    private EditText enterName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // получаем id кнопки
+        myButton = findViewById(R.id.button);
+        enterName = findViewById(R.id.editText);
+        // устанавливаем обработчик события
+        myButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               // получаем значение текста из поля
+               String name = enterName.getText().toString();
+               // устанавливаем значение текста
+               showtext.setText("Hello, " + name);
+           }
+        });
+
+
+        showtext = findViewById(R.id.textView);
+
     }
 }
