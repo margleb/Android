83f9e2f0ad1ae169af5ec54438e514c17c253ecd
margleb <margleb@yandex.ru>
2020-03-27 17:05:15 +0300

    Adding an Onclick Event in the XML

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button showMoney;
+    private Button showTag;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showMoney = findViewById(R.id.button_make_rain);
+        showTag = findViewById(R.id.button_show_tag);
+
+        // showMoney.setOnClickListener(new View.OnClickListener() {
+            // @Override
+            // public void onClick(View v) {
+                // Log.d("MYTAG", "onClick: Show Money");
+                // System.out.println("Hello!");
+            // }
+        // });
     }
+
+    // работает, благодаря указанаму атрибуту onclick в xml
+    public void showTag(View v) {
+        // по идеи должен выводить информацию в лог по событию onClick
+        Log.d("MYTAG", "onClick: Show Money");
+        // System.out.println("Hello!");
+    }
+
+    public void makeItRain(View v) {
+        Log.d("MIR", "makeItRain: Trapped");
+        // System.out.println("Hello!");
+    }
+
+
 }
