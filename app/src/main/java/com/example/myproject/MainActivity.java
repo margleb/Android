aef5da356986351fbc136bd52308f2358b69ca2b
margleb <margleb@yandex.ru>
2020-03-31 12:06:45 +0300

    How to Use Android SharedPreferences

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+    private static final String MESSAGE_ID = "messages_prefs";
+    private TextView textInfo;
+    private Button saveButton;
+    private TextView enterMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textInfo = findViewById(R.id.text_info);
+        saveButton = findViewById(R.id.saveButton);
+        enterMessage = findViewById(R.id.enter_message);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              String message = enterMessage.getText().toString().trim();
+              // mode_private - гарантирует что только это приложение сможет использовать данную настройку
+              SharedPreferences sharedPreferences = getSharedPreferences(MESSAGE_ID, MODE_PRIVATE);
+              SharedPreferences.Editor editor = sharedPreferences.edit();
+              editor.putString("message", message);
+              editor.apply(); // сохраняем на диск
+            }
+        });
+        // Get Data back from SP
+        SharedPreferences getShareData = getSharedPreferences(MESSAGE_ID, MODE_PRIVATE);
+        String value = getShareData.getString("message", "Nothing yet");
+        textInfo.setText(value);
     }
+
 }
