17c0b2597b7458aa7f16dd0135af4abec393d1fa
margleb <margleb@yandex.ru>
2020-04-02 10:04:13 +0300

    Database Handler Class Setup - Part 2

--- app/src/main/java/com/example/myproject/data/DatabaseHandler.java
+++ app/src/main/java/com/example/myproject/data/DatabaseHandler.java
@@ -3,19 +3,32 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.myproject.R;
+import com.example.myproject.util.Util;
+
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context ) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
     }
 
+    // We create our table
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        //SQL - Structured Query Language
+        // create table_name (id, name, phone_number);
+        String CREATE_CONTACT_TABLE  = "CREATE TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY," + Util.KEY_NAME + " TEXT, "
+                + Util.KEY_PHONE_NUMBER + " TEXT" + ")";
+        db.execSQL(CREATE_CONTACT_TABLE); // creating our table
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        String DROP_TABLE = String.valueOf(R.string.db_drop);
+        db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
+        // Create a table again
+        onCreate(db);
     }
+    
 }
--- app/src/main/java/com/example/myproject/util/Util.java
+++ app/src/main/java/com/example/myproject/util/Util.java
@@ -11,5 +11,5 @@ public class Util {
     public static final String KEY_NAME = "name";
     public static final String KEY_PHONE_NUMBER = "phone_number";
 
-    
+
 }
