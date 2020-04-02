b66f5a9f961ba5775e912ed7522cfbe0fec12fe0
margleb <margleb@yandex.ru>
2020-04-02 16:48:46 +0300

    Showing Data in a Listview

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.myproject.data.DatabaseHandler;
+import com.example.myproject.model.Contact;
+import com.example.myproject.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayList<String> contactArrayList;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list_view);
+        contactArrayList = new ArrayList<>();
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+//        db.addContact(new Contact("Boris", "89113335018"));
+//        db.addContact(new Contact("Vadim", "89119318038"));
+//        db.addContact(new Contact("Valentin", "89111431248"));
+//        db.addContact(new Contact("Valeriy", "89115532342"));
+
+         List<Contact> contacts = db.getAllContacts();
+         for(Contact contact : contacts) {
+             Log.d("Contacts", contact.toString());
+             contactArrayList.add(contact.getName());
+         }
+         // create array adapter
+        arrayAdapter = new ArrayAdapter<>(
+                this,
+                 android.R.layout.simple_list_item_1,
+                 contactArrayList
+        );
+
+        // add to our listview
+        listView.setAdapter(arrayAdapter);
+
+        // Atttach eventlistener to listview
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("List", "onItemClick: " + contactArrayList.get(position));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myproject/util/Util.java
@@ -0,0 +1,15 @@
+package com.example.myproject.util;
+
+public class Util {
+    // Database realtred items
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "contact_db";
+    public static final String TABLE_NAME = "contacts";
+
+    // Contacts table colums
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PHONE_NUMBER = "phone_number";
+
+
+}
