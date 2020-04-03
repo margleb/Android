bcf722797cd42bb6dc0b1b30f8be1a1e0ac41a76
margleb <margleb@yandex.ru>
2020-04-03 21:19:50 +0300

    CRUD Operations - Get Item

--- app/src/main/java/com/bawp/babyneeds/data/DatabaseHandler.java
+++ app/src/main/java/com/bawp/babyneeds/data/DatabaseHandler.java
@@ -1,9 +1,15 @@
 package com.bawp.babyneeds.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 import androidx.annotation.Nullable;
+
+import com.bawp.babyneeds.model.Item;
 import com.bawp.babyneeds.util.Contstants;
 
 
@@ -34,6 +40,43 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
     // CRUD operations
+    public void addItem(Item item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Contstants.KEY_BABY_ITEM, item.getItemName());
+        values.put(Contstants.KEY_COLOR, item.getItemColor());
+        values.put(Contstants.KEY_QTY_NUMBER, item.getItemQuantity());
+        values.put(Contstants.KEY_ITEM_SIZE, item.getItemSize());
+        values.put(Contstants.KEY_DATE_NAME, java.lang.System.currentTimeMillis()); // timestamp in milliseconds
+        // Insert the raw
+        db.insert(Contstants.TABLE_NAME, null, values);
+        Log.d("DBHandler", "added Item: ");
+    }
 
+    public Item getItem(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(Contstants.TABLE_NAME,
+                new String[]{
+                Contstants.KEY_ID,
+                Contstants.KEY_BABY_ITEM,
+                Contstants.KEY_QTY_NUMBER,
+                Contstants.KEY_ITEM_SIZE,
+                Contstants.KEY_DATE_NAME},
+                Contstants.KEY_ID + "=?",
+                new String[] {String.valueOf(id)}, null, null, null, null);
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        Item item = new Item();
+        if(cursor != null) {
+            item.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(Contstants.KEY_ID))));
+            item.setItemName(cursor.getString(cursor.getColumnIndex(Contstants.KEY_BABY_ITEM)));
+            item.setItemQuantity(cursor.getInt(cursor.getColumnIndex(Contstants.KEY_QTY_NUMBER)));
+            item.setItemSize(cursor.getInt(cursor.getColumnIndex(Contstants.KEY_ITEM_SIZE)));
+            // convert Timestamp to something readeble
+            
+        }
+        return null;
+    }
 
 }
