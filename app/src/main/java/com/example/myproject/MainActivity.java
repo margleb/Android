3cf76934026bd7929d654dae9a52fa6a2f323456
margleb <margleb@yandex.ru>
2020-04-03 12:54:16 +0300

    Adding OnClick Listener

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -1,21 +1,13 @@
 package com.example.myproject;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 import com.example.myproject.adapter.RecycleViewAdapter;
 import com.example.myproject.data.DatabaseHandler;
 import com.example.myproject.model.Contact;
-import com.example.myproject.util.Util;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 1. получаем обработчик на обьект
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
+        // 2. cоединяем с layoutManager
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         contactArrayList = new ArrayList<>();
@@ -42,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             contactArrayList.add(contact);
         }
 
-        // setup adapter
+        // соединяем адаптер с данными для отображения
         recycleViewAdapter = new RecycleViewAdapter(MainActivity.this, contactArrayList);
         recyclerView.setAdapter(recycleViewAdapter);
     }
--- app/src/main/java/com/example/myproject/adapter/RecycleViewAdapter.java
+++ app/src/main/java/com/example/myproject/adapter/RecycleViewAdapter.java
@@ -1,9 +1,11 @@
 package com.example.myproject.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -26,31 +28,54 @@ public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.
 
     @NonNull
     @Override
+    // 1. Создает новое представление (вызывается layout менеджером)
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        // создает новый вид
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contact_row, viewGroup, false);
         return new ViewHolder(view);
     }
 
     @Override
+    // 2. заменяет контент в представлении (вызывается layout менеджером)
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-        Contact contact = contactList.get(position); // each contact object inside of our list
+        // получает индекс элемента из базы данны
+        Contact contact = contactList.get(position);
+        // заменяет содержимое представления этим элементом
         viewHolder.contactName.setText(contact.getName());
         viewHolder.phoneNumber.setText(contact.getPhoneNumber());
     }
 
     @Override
+    // возращает разамер данных в базе (вызывается layout менеджером)
     public int getItemCount() {
         return contactList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
+    // 3. предоставляет ссылки на views для каждого элемента data
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public TextView contactName;
         public TextView phoneNumber;
+        public ImageView icon_button;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            // itemView.setOnClickListener(this);
             contactName = itemView.findViewById(R.id.name);
             phoneNumber = itemView.findViewById(R.id.phone_number);
+            icon_button = itemView.findViewById(R.id.icon_button);
+            icon_button.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // получаем позицию
+            int position = getAdapterPosition();
+            Contact contact = contactList.get(position);
+            switch(v.getId()) {
+                case R.id.icon_button:
+                    Log.d("IconClicked", "onClick: " + contact.getPhoneNumber());
+                    break;
+            }
+            Log.d("Clicked", "onClick: " + contact.getName());
         }
     }
 }
