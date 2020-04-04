35e4fdb7bcb41ba7535411b56d50fa35304aff9e
margleb <margleb@yandex.ru>
2020-04-04 19:20:20 +0300

    Making FAB Button Work

--- app/src/main/java/com/bawp/babyneeds/ListActivity.java
+++ app/src/main/java/com/bawp/babyneeds/ListActivity.java
@@ -1,15 +1,23 @@
 package com.bawp.babyneeds;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.bawp.babyneeds.data.DatabaseHandler;
 import com.bawp.babyneeds.model.Item;
 import com.bawp.babyneeds.ui.RecycleViewsAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,14 +28,21 @@ public class ListActivity extends AppCompatActivity {
     private RecycleViewsAdapter recyclerViewsAdapter;
     private List<Item> itemList;
     private DatabaseHandler databaseHandler;
-
-
+    private FloatingActionButton fab;
+    private AlertDialog.Builder builder;
+    private AlertDialog alertDialog;
+    private Button saveButton;
+    private EditText babyItem;
+    private EditText itemQuantity;
+    private EditText itemColor;
+    private EditText itemSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         recyclerView = findViewById(R.id.recyclerview);
+        fab = findViewById(R.id.fab);
         databaseHandler = new DatabaseHandler(this);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -41,5 +56,65 @@ public class ListActivity extends AppCompatActivity {
         recyclerViewsAdapter = new RecycleViewsAdapter(this, itemList);
         recyclerView.setAdapter(recyclerViewsAdapter);
         recyclerViewsAdapter.notifyDataSetChanged();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createPopDialog();
+            }
+        });
+    }
+
+    private void createPopDialog() {
+        builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup, null);
+        babyItem = view.findViewById(R.id.babyItem);
+        itemQuantity = view.findViewById(R.id.itemQuantity);
+        itemColor = view.findViewById(R.id.itemColor);
+        itemSize = view.findViewById(R.id.itemSize);
+        saveButton = view.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // view передается для Snackbar
+                if(!babyItem.getText().toString().isEmpty()
+                        && !itemColor.getText().toString().isEmpty()
+                        && !itemQuantity.getText().toString().isEmpty()
+                        && !itemSize.getText().toString().isEmpty()) {
+                    saveItem(v);
+                } else {
+                    Snackbar.make(v, "Empty Fields not Allowed!", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder.setView(view);
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void saveItem(View view) {
+        // Todo: save each baby item to db
+        Item item = new Item();
+        String newItem = babyItem.getText().toString().trim();
+        String newColor = itemColor.getText().toString().trim();
+        int quantity = Integer.parseInt(itemQuantity.getText().toString().trim());
+        int size = Integer.parseInt(itemSize.getText().toString().trim());
+        item.setItemName(newItem);
+        item.setItemColor(newColor);
+        item.setItemQuantity(quantity);
+        item.setItemSize(size);
+
+        databaseHandler.addItem(item);
+        Snackbar.make(view, "Item Saved", Snackbar.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //code to be run
+                alertDialog.dismiss();
+                // Todo: move to next screen - details screen
+                startActivity(new Intent(ListActivity.this, ListActivity.class));
+                finish();
+            }
+        }, 1200); // 1 sec
     }
 }
