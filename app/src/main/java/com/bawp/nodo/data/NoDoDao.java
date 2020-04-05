090448389813edab1164de35f7c1c7ccf64734c0
margleb <margleb@yandex.ru>
2020-04-05 11:29:35 +0300

    Creating the NoDoDao - Data Access Object Class

---
+++ app/src/main/java/com/bawp/nodo/data/NoDoDao.java
@@ -0,0 +1,28 @@
+package com.bawp.nodo.data;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.bawp.nodo.model.NoDo;
+
+import java.util.List;
+
+@Dao
+public interface NoDoDao {
+    //CRUD
+    @Insert
+    void insert(NoDo noDo);
+
+    @Query("DELETE FROM nodo_table")
+    void deleteAll();
+
+    @Query("DELETE FROM nodo_table WHERE id = :id")
+    int deleteANoDo(int id);
+
+    @Query("UPDATE nodo_table SET nodo_col = :nodoText WHERE id = :id")
+    int updateNoDoItem(int id, String nodoText);
+
+    @Query("SELECT * FROM nodo_table ORDER BY nodo_col DESC")
+    List<NoDo> getAllNoDos();
+}
--- app/src/main/java/com/bawp/nodo/model/NoDo.java
+++ app/src/main/java/com/bawp/nodo/model/NoDo.java
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "nod_table")
+@Entity(tableName = "nodo_table")
 public class NoDo {
     @PrimaryKey(autoGenerate = true)
     private int id;
