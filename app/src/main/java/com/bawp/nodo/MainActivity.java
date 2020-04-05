d649bf4cb43873f4586d2d99154ee6e20a1fcc14
margleb <margleb@yandex.ru>
2020-04-05 16:18:44 +0300

    Creating the NoDo Activity

--- app/src/main/java/com/bawp/nodo/MainActivity.java
+++ app/src/main/java/com/bawp/nodo/MainActivity.java
@@ -2,11 +2,14 @@ package com.bawp.nodo;
 
 import android.os.Bundle;
 
+import com.bawp.nodo.ui.NoDoListAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
@@ -14,12 +17,20 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NoDoListAdapter noDoListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+        noDoListAdapter = new NoDoListAdapter(this);
+        recyclerView.setAdapter(noDoListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/bawp/nodo/data/NoDoRoomDatabase.java
+++ app/src/main/java/com/bawp/nodo/data/NoDoRoomDatabase.java
@@ -1,10 +1,14 @@
 package com.bawp.nodo.data;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.RoomDatabase.Callback;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.bawp.nodo.model.NoDo;
 
@@ -12,17 +16,50 @@ import com.bawp.nodo.model.NoDo;
 public abstract class NoDoRoomDatabase extends RoomDatabase {
     // не кишируется и не сохраняется
     private static volatile NoDoRoomDatabase INSTANCE;
+
     public abstract NoDoDao noDoDao();
 
     public static NoDoRoomDatabase getDatabase(final Context context) {
-        if(INSTANCE == null) {
+        if (INSTANCE == null) {
             synchronized (NoDoRoomDatabase.class) {
-                if(INSTANCE == null) {
+                if (INSTANCE == null) {
                     // create our db
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), NoDoRoomDatabase.class, "nodo_database").build();
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), NoDoRoomDatabase.class, "nodo_database")
+                            .addCallback(roomDatabaseCallBack)
+                            .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback roomDatabaseCallBack = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final NoDoDao noDoDao;
+
+        public PopulateDbAsync(NoDoRoomDatabase db) {
+            noDoDao = db.noDoDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noDoDao.deleteAll(); // removes all itemds from our tables
+            // for testing
+            NoDo noDo = new NoDo("Buy an new Ferriari");
+            noDoDao.insert(noDo);
+
+            noDo = new NoDo("Buy Big house");
+            noDoDao.insert(noDo);
+
+            return null;
+        }
     }
 }
