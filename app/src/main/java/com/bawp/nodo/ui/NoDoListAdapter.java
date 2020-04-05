df4b11cf2b8648f8fd30e5b52ea929b116f5d311
margleb <margleb@yandex.ru>
2020-04-05 15:36:23 +0300

    Creating RecyclerView Adapter

---
+++ app/src/main/java/com/bawp/nodo/ui/NoDoListAdapter.java
@@ -0,0 +1,61 @@
+package com.bawp.nodo.ui;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bawp.nodo.R;
+import com.bawp.nodo.model.NoDo;
+import java.util.List;
+
+
+public class NoDoListAdapter extends RecyclerView.Adapter<NoDoListAdapter.NoDoViewHolder> {
+
+    private final LayoutInflater noDoInflater;
+    private List<NoDo> noDoList; // cached copy of nodo items
+
+    public NoDoListAdapter(Context context) {
+        this.noDoInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public NoDoViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = noDoInflater.inflate(R.layout.recyclerview_item, viewGroup, false);
+        return new NoDoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoDoViewHolder noDoViewHolder, int position) {
+        if(noDoList != null) {
+            NoDo current = noDoList.get(position);
+            noDoViewHolder.noDoTextView.setText(current.getNoDo());
+        } else {
+            noDoViewHolder.noDoTextView.setText(R.string.no_notodo);
+        }
+    }
+
+    void setNoDo(List<NoDo> noDos) {
+        noDoList = noDos;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if(noDoList != null) {
+            return noDoList.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public class NoDoViewHolder extends RecyclerView.ViewHolder {
+        public TextView noDoTextView;
+        public NoDoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            noDoTextView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
