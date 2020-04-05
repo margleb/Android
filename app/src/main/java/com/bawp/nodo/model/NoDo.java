8e621d0a6670c4152b9e74bdafccf4648cea15e9
margleb <margleb@yandex.ru>
2020-04-05 11:20:08 +0300

    Creating ROOM NoDo App - Setup and Entity Class

---
+++ app/src/main/java/com/bawp/nodo/model/NoDo.java
@@ -0,0 +1,34 @@
+package com.bawp.nodo.model;
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "nod_table")
+public class NoDo {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "nodo_col")
+    private String noDo;
+
+    public NoDo(@NonNull String noDo) {
+        this.noDo = noDo;
+    }
+
+    public String getNoDo(){
+        return noDo;
+    }
+
+    public void setNoDo(@NonNull  String noDo) {
+        this.noDo = noDo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
