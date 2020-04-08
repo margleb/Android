4b2c1895e51a836e691be74bb756a684302ec31a
margleb <margleb@yandex.ru>
2020-04-08 20:41:56 +0300

    CHALLENGE Solution - Creating MarkerOptions

--- app/src/main/java/com/bawp/myapplication/MapsActivity.java
+++ app/src/main/java/com/bawp/myapplication/MapsActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,12 +25,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LatLng mountainEverest = new LatLng(27.9881388,86.9162203);
     private LatLng mountainKilimanjaro = new LatLng(-3.0674031,37.3468725);
     private LatLng mountainAlps = new LatLng(45.830072,6.1828847);
-    private ArrayList<Marker> markerArrayList;
 
-    // Todo: Create Markers for each mountain
-    private Marker everestMarker;
-    private Marker kelimanjaroMarker;
-    private Marker theAlpsMarker;
+    private MarkerOptions everestOptions;
+    private MarkerOptions theAlpsOptions;
+    private MarkerOptions theKilimanjaroOptions;
+
+    private List<MarkerOptions> markerOptionsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,51 +43,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        markerArrayList = new ArrayList<>();
+        markerOptionsList = new ArrayList<>();
 
-        kelimanjaroMarker = mMap.addMarker(new MarkerOptions()
-                .position(mountainKilimanjaro)
-                .title("Mt Kelimanjaro")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-        markerArrayList.add(kelimanjaroMarker);
-        theAlpsMarker = mMap.addMarker(new MarkerOptions()
-                .position(mountainAlps)
-                .title("The Alps")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
-        markerArrayList.add(theAlpsMarker);
-        everestMarker = mMap.addMarker(new MarkerOptions()
-                .position(mountainEverest)
-                .title("Mt. Everest")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+        everestOptions = new MarkerOptions().position(mountainEverest).
+                title("Everest").
+                icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+        markerOptionsList.add(everestOptions);
 
-        markerArrayList.add(everestMarker);
+        theAlpsOptions = new MarkerOptions().position(mountainAlps).
+                title("Alps").
+                icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        markerOptionsList.add(theAlpsOptions);
 
-        for(Marker marker : markerArrayList) {
-            LatLng latLng = new LatLng(marker.getPosition().latitude, marker.getPosition().longitude);
-            mMap.addMarker(new MarkerOptions().position(latLng));
+        theKilimanjaroOptions = new MarkerOptions().position(mountainKilimanjaro).
+                title("Alps").
+                icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        markerOptionsList.add(theKilimanjaroOptions);
+
+        for(MarkerOptions options : markerOptionsList) {
+            LatLng latLng = new LatLng(options.getPosition().latitude, options.getPosition().longitude);
+            mMap.addMarker(options);
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 4));
         }
-
-        // Add a marker in Sydney and move the camera
-//        LatLng winterPalace = new LatLng(59.9403985,30.3116075);
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(winterPalace).title("Marker of Winter Palace at St.Petersbourg")
-//                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
-//                .alpha(0.8f));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(winterPalace, 9)); // 1 - 20
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(winterPalace));
     }
 }
