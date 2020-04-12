b75759614db5822834f19d4304cfa0df23c085f5
margleb <margleb@yandex.ru>
2020-04-12 12:45:07 +0300

    Playing Video with SurfaceView

--- app/src/main/java/com/bawp/soundpool/MainActivity.java
+++ app/src/main/java/com/bawp/soundpool/MainActivity.java
@@ -2,72 +2,79 @@ package com.bawp.soundpool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.AudioAttributes;
-import android.media.SoundPool;
-import android.os.Build;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button buttonOne, buttonTwo, buttonThree;
-    private SoundPool soundPool;
-    private int sound1, sound2, sound3;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SurfaceHolder.Callback {
+    Button play, pause, skip;
+    MediaPlayer mediaPlayer;
+    SurfaceView surfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonOne = findViewById(R.id.button_one);
-        buttonTwo = findViewById(R.id.button_two);
-        buttonThree = findViewById(R.id.button_three);
+        mediaPlayer = mediaPlayer.create(MainActivity.this, R.raw.video);
 
-        buttonOne.setOnClickListener(this);
-        buttonTwo.setOnClickListener(this);
-        buttonThree.setOnClickListener(this);
+        surfaceView = findViewById(R.id.surface);
+        surfaceView.setKeepScreenOn(true);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    // тип контента
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    // для целей использования
-                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
-                    .build();
+        SurfaceHolder holder = surfaceView.getHolder();
+        holder.setFixedSize(400, 300);
+        holder.addCallback(this);
 
-            soundPool = new SoundPool.Builder()
-                    // кол-во загружаемых файлов
-                    .setMaxStreams(4)
-                    .setAudioAttributes(audioAttributes)
-                    .build();
+        play = findViewById(R.id.button_blay);
+        pause = findViewById(R.id.button_pause);
+        skip = findViewById(R.id.button_skip);
 
-            sound1 = soundPool.load(MainActivity.this, R.raw.click_one, 1);
-            sound2 = soundPool.load(MainActivity.this, R.raw.click_two, 1);
-            sound3 = soundPool.load(MainActivity.this, R.raw.click_three, 1);
-        }
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        skip.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_one:
-                soundPool.play(sound1, 1,1,1,0,1);
-                break;
-            case R.id.button_two:
-                soundPool.play(sound2, 1,1,1,0,1);
-                break;
-            case R.id.button_three:
-                soundPool.play(sound3, 1,1,1,0,1);
-                break;
+        switch(v.getId()) {
+            case R.id.button_blay:
+                mediaPlayer.start();
+              break;
+            case R.id.button_pause:
+                mediaPlayer.pause();
+              break;
+            case R.id.button_skip:
+                mediaPlayer.seekTo(mediaPlayer.getDuration() / 2);
+              break;
         }
     }
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mediaPlayer.setDisplay(holder);
+    }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(soundPool != null) {
-            soundPool.release();
-            soundPool = null;
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mediaPlayer != null) {
+            mediaPlayer.pause();
+            mediaPlayer.release();
+            mediaPlayer = null;
         }
     }
 }
