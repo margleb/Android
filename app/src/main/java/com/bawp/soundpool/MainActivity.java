dae0d3704d88a9a9e49f6ee245795316d56cb4db
margleb <margleb@yandex.ru>
2020-04-12 10:03:03 +0300

    Setting up SoundPool

--- app/src/main/java/com/bawp/soundpool/MainActivity.java
+++ app/src/main/java/com/bawp/soundpool/MainActivity.java
@@ -1,13 +1,39 @@
 package com.bawp.soundpool;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button_one, button_two, button_three, button_four;
+    private SoundPool soundPool;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_one = findViewById(R.id.button_one);
+        button_two = findViewById(R.id.button_two);
+        button_three = findViewById(R.id.button_three);
+        button_four = findViewById(R.id.button_four);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    // тип контента
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    // для целей использования
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    // кол-во загружаемых файлов
+                    .setMaxStreams(4)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        }
     }
 }
