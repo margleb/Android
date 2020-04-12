ba82b4d2bfe72e81dbc8ce404054c5a3f86f3b7c
margleb <margleb@yandex.ru>
2020-04-12 17:45:03 +0300

    Android Fading Animation

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AnimationDrawable batAnimation;
+    // private AnimationDrawable batAnimation;
     private ImageView batImage;
 
     @Override
@@ -19,18 +21,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         batImage = findViewById(R.id.bat_anim);
-        batImage.setBackgroundResource(R.drawable.bat_anim);
-        batAnimation = (AnimationDrawable) batImage.getBackground();
+        // batImage.setBackgroundResource(R.drawable.bat_anim);
+        // batAnimation = (AnimationDrawable) batImage.getBackground();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        batAnimation.start();
+        // batAnimation.start();
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                batAnimation.stop();
+                Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein_animation);
+                batImage.startAnimation(startAnimation);
+                // batAnimation.stop();
             }
         }, 3000);
         return super.onTouchEvent(event);
