df047b83f8e0a997e65f6baf4938dc9cd5053922
margleb <margleb@yandex.ru>
2020-04-13 16:19:31 +0300

    Android Fragment from Code

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.myContainer);
+
+        if(fragment == null) {
+            fragment = new MainFragment();
+            fragmentManager.beginTransaction().add(R.id.myContainer, fragment).commit();
+        }
     }
 }
