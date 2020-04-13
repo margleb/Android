2c11091c0d6655f9202e8874fb7a14cea6de6552
margleb <margleb@yandex.ru>
2020-04-13 22:11:40 +0300

    Intro to List Fragments

--- app/src/main/java/com/bawp/myapplication/MainActivity.java
+++ app/src/main/java/com/bawp/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.bawp.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.myContainer);
+
+        if(fragment == null) {
+            fragment = new myFragment();
+            fm.beginTransaction().add(R.id.myContainer, fragment).commit();
+        }
+
     }
 }
