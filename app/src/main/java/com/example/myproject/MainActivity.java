149669a3f06e2fda5804f7bf688f57114da8f1b2
margleb <margleb@yandex.ru>
2020-04-13 12:38:16 +0300

    Android Implicit Intents - Sending Message via Email

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button send_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        send_button = findViewById(R.id.send_button);
+
+        send_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("Text/Plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Implict intent");
+                intent.putExtra(Intent.EXTRA_TEXT, "This is message from implict intent!");
+                startActivity(intent);
+            }
+        });
+
     }
 }
