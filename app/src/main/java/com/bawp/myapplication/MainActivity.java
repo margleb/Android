1ccdff6f1602e8cd0225f4263b2786bc29418f18
margleb <margleb@yandex.ru>
2020-04-15 12:15:23 +0300

    Finalized Fragment App

--- app/src/main/java/com/bawp/myapplication/MainActivity.java
+++ app/src/main/java/com/bawp/myapplication/MainActivity.java
@@ -12,26 +12,37 @@ import com.bawp.myapplication.data.Course;
 
 public class MainActivity extends AppCompatActivity implements FragmentListCourse.Callbacks  {
 
+    private boolean isTwoPane = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FragmentManager fm = getSupportFragmentManager();
-//        Fragment fragment = fm.findFragmentById(R.id.myContainer);
-//
-//        if(fragment == null) {
-//            fragment = new FragmentListCourse();
-//            fm.beginTransaction().add(R.id.myContainer, fragment).commit();
-//        }
+        if(findViewById(R.id.detailContainer) != null) {
+            isTwoPane = true;
+        }
+
+
 
     }
 
     @Override
     public void onItemSelected(Course course, int position) {
-        // Toast.makeText(MainActivity.this, course.getCourseName(), Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, CourseDetailActivity.class);
-        intent.putExtra("course_id", position);
-        startActivity(intent);
+        if(isTwoPane) {
+        Bundle bundle = new Bundle();
+        bundle.putInt("course_id", position);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentDetailCourse fragment = new FragmentDetailCourse();
+        fragment.setArguments(bundle);
+        fm.beginTransaction().replace(R.id.detailContainer, fragment).commit();
+
+        } else {
+            // Toast.makeText(MainActivity.this, course.getCourseName(), Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, CourseDetailActivity.class);
+            intent.putExtra("course_id", position);
+            startActivity(intent);
+        }
     }
 }
