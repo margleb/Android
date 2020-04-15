093d8b579c67e7ddc5b43eb385080ae683e98fd0
margleb <margleb@yandex.ru>
2020-04-15 09:30:49 +0300

    Setup DetailsActivity and Fragment - Part 1

---
+++ app/src/main/java/com/bawp/myapplication/CourseDetailActivity.java
@@ -0,0 +1,20 @@
+package com.bawp.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class CourseDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_detail);
+        if(savedInstanceState == null) {
+            CourseDetailFragment fragment = new CourseDetailFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().add(R.id.detailContainer, fragment).commit();
+        }
+    }
+}
