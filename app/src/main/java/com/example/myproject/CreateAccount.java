5981da6e5ac3f22441b908e7567817a1ea7c0ff1
margleb <margleb@yandex.ru>
2020-04-16 17:15:56 +0300

    Setup Create Account with Firebase

--- app/src/main/java/com/example/myproject/CreateAccount.java
+++ app/src/main/java/com/example/myproject/CreateAccount.java
@@ -3,12 +3,42 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreateAccount extends AppCompatActivity {
+    // авторизация
+    private FirebaseAuth mAuth;
+    // слушатель собывтия авторизации
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // текущий авторизирвоанный пользователь
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore firebaseFireStore = FirebaseFirestore.getInstance();
+
+    private TextView username_account;
+    private AutoCompleteTextView email_account;
+    private EditText password_account;
+    private Button create_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        // инициализируем авторизацию
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 }
