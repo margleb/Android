fea866c582bd24625795ab2bc255feb0aaa542f0
margleb <margleb@yandex.ru>
2020-04-16 15:21:08 +0300

    Setup Create Account Activity

--- app/src/main/java/com/example/myproject/LoginAccount.java
+++ app/src/main/java/com/example/myproject/LoginAccount.java
@@ -1,12 +1,29 @@
 package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginAccount extends AppCompatActivity {
+    private Button login_button;
+    private Button create_acc_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__account);
+        login_button = findViewById(R.id.email_sign_in_button);
+        create_acc_button = findViewById(R.id.create_acc_button);
+
+        create_acc_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginAccount.this, CreateAccount.class));
+            }
+        });
+
     }
 }
