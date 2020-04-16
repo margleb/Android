e066d247961a634544b47e4e5733a41abfa9adaa
margleb <margleb@yandex.ru>
2020-04-16 10:00:27 +0300

    Creating Multiple Documents and Retrieving Multiple Documents

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -15,12 +15,16 @@ import android.widget.Toast;
 import com.example.myproject.data.Journal;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.model.DocumentCollections;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -37,14 +41,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     // подключение
     FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-    private DocumentReference Journal = firebaseFirestore.collection("Journal").document("Thought");
+    private DocumentReference JournalThoughtData = firebaseFirestore.collection("Journal").document("Thought");
+    private CollectionReference JournalData =  firebaseFirestore.collection("Journal");
 
     private static final String TAG = "ErrorFireStore";
 
     @Override
     protected void onStart() {
         super.onStart();
-        Journal.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+        JournalThoughtData.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
                 if(documentSnapshot != null && documentSnapshot.exists()) {
@@ -88,65 +93,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String note = add_note.getText().toString().trim();
-                String thought = add_thought.getText().toString().trim();
-                // Map<String, Object> data = new HashMap<>();
-                // data.put(KEY_NOTE, note);
-                // data.put(KEY_THOUGHT, thought);
-
-                Journal journal = new Journal();
-                journal.set_note(note);
-                journal.set_thought(thought);
-
-                Journal
-                        .set(journal).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Toast.makeText(MainActivity.this, "Sucess!", Toast.LENGTH_SHORT).show();
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(TAG, "onFailure: " + e.getStackTrace());
-                        }
-                    });
+                addToJournal();
             }
         });
 
-        show_button.setOnClickListener(new View.OnClickListener() {
+         show_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Journal
-                        .get()
-                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                            @Override
-                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                if(documentSnapshot != null && documentSnapshot.exists()) {
-
-                                    Journal journal = documentSnapshot.toObject(Journal.class);
-                                    if(journal != null) {
-                                        show_note.setText(journal.get_note());
-                                        show_note.setText(journal.get_thought());
-                                    }
-
-                                    // String note = documentSnapshot.getString(KEY_NOTE);
-                                    // String thought = documentSnapshot.getString(KEY_THOUGHT);
-                                    // show_note.setText(note);
-                                    // show_thought.setText(thought);
-                                }
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                 Log.d(TAG, "onFailure: " + e.getStackTrace());
-                            }
-                });
+                ShowFromJournal();
             }
         });
-
-
-
     }
+
+    public void addToJournal() {
+        String note = add_note.getText().toString().trim();
+        String thought = add_thought.getText().toString().trim();
+        Journal journal = new Journal(note, thought);
+        // journal.set_note(note);
+        // journal.set_thought(thought);
+        JournalData.add(journal);
+    }
+
+    public void ShowFromJournal() {
+        JournalData.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for(QueryDocumentSnapshot snapshots : queryDocumentSnapshots) {
+                    Log.d("DoucmentId", "onSuccess: " + snapshots.getId());
+                }
+            }
+        });
+    }
+
+
 
     @Override
     public void onClick(View v) {
@@ -160,7 +139,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void deleteData() {
-        Journal.update(KEY_NOTE, FieldValue.delete());
+        JournalThoughtData.update(KEY_NOTE, FieldValue.delete());
         // Journal.delete();
     }
 
@@ -171,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         data.put(KEY_NOTE, note);
         data.put(KEY_THOUGHT, thought);
 
-        Journal.update(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+        JournalThoughtData.update(data).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
                 Toast.makeText(MainActivity.this, "Updatet Success!", Toast.LENGTH_SHORT);
