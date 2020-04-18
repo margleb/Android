715a50c2c30061946afc76af40b12fd9525598a3
margleb <margleb@yandex.ru>
2020-04-18 17:29:51 +0300

    RecyclerView Adapter and TimeAgo - Part 1

---
+++ app/src/main/java/com/example/myproject/ui/JournalRecycleViewAdapter.java
@@ -0,0 +1,59 @@
+package com.example.myproject.ui;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myproject.R;
+import com.example.myproject.model.Journal;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class JournalRecycleViewAdapter extends RecyclerView.Adapter<JournalRecycleViewAdapter.ViewHolder> {
+    private Context context;
+    private List<Journal> journalList;
+
+    @NonNull
+    @Override
+    public JournalRecycleViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LinearLayout.inflate(context, R.layout.journal_row, parent);
+        return new ViewHolder(view);
+        // return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull JournalRecycleViewAdapter.ViewHolder holder, int position) {
+        Journal journal = journalList.get(position);
+        holder.journal_title_list.setText(journal.getTitle());
+        holder.journal_thought_list.setText(journal.getDescription());
+        // пикассо плагин
+        Picasso.get().load(journal.getImageUrl()).placeholder(R.drawable.image_three).into(holder.journal_image_view);
+        holder.journal_timestamp_list.setText(DateUtils.getRelativeTimeSpanString(journal.getTimeAdded().getSeconds() * 1000));
+    }
+
+    @Override
+    public int getItemCount() {
+        return journalList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView journal_image_view;
+        private TextView journal_title_list, journal_thought_list, journal_timestamp_list;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            journal_image_view = itemView.findViewById(R.id.journal_image_view);
+            journal_title_list = itemView.findViewById(R.id.journal_title_list);
+            journal_thought_list = itemView.findViewById(R.id.journal_thought_list);
+            journal_timestamp_list = itemView.findViewById(R.id.journal_timestamp_list);
+        }
+    }
+}
