2340f5e1be518027ac375f5fba5cfe4181491cdc
margleb <margleb@yandex.ru>
2020-04-19 12:42:22 +0300

    Journal App - Final Product

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.myproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.myproject.util.JournalApi;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import javax.annotation.Nullable;
+
 public class MainActivity extends AppCompatActivity {
     private Button start_button;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuth != null) {
+            mAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +53,38 @@ public class MainActivity extends AppCompatActivity {
         start_button = findViewById(R.id.start_button);
         getSupportActionBar().setElevation(0);
 
+        mAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser != null) {
+                    String currentUserId = currentUser.getUid();
+                    collectionReference.whereEqualTo("userId", currentUserId).addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                            if (e != null) {
+                                // Log.d("QueryError", e.printStackTrace().toString());
+                            }
+                            if (!queryDocumentSnapshots.isEmpty()) {
+                                for (DocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                    JournalApi journalApi = JournalApi.getInstance();
+                                    journalApi.setUserId(snapshot.getString("userId"));
+                                    journalApi.setUserId(snapshot.getString("username"));
+                                    startActivity(new Intent(MainActivity.this, JournalListActivity.class));
+                                    finish();
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        };
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, LoginAccount.class));
+                finish();
             }
         });
     }
