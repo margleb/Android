bf1e5b578bbbc67b2499d700e48a610870d280d6
margleb <margleb@yandex.ru>
2020-04-21 13:40:18 +0300

    analyzing Image - Part 1

--- app/src/main/java/com/bawp/myproject/MainActivity.java
+++ app/src/main/java/com/bawp/myproject/MainActivity.java
@@ -1,22 +1,36 @@
 package com.bawp.myproject;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.Facing;
 import com.otaliastudios.cameraview.Frame;
 import com.otaliastudios.cameraview.FrameProcessor;
+import com.theartofdev.edmodo.cropper.CropImage;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements FrameProcessor {
     private Facing cameraFacing = Facing.FRONT;
@@ -45,7 +59,63 @@ public class MainActivity extends AppCompatActivity implements FrameProcessor {
         faceDetectionCameraView.setLifecycleOwner(MainActivity.this);
         faceDetectionCameraView.addFrameProcessor(MainActivity.this);
 
+        bottomSheetButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().start(MainActivity.this);
+            }
+        });
+        bottomSheetRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        bottomSheetRecyclerView.setAdapter(new FaceDetectionAdapter(faceDetectionModels, this));
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK) {
+                Uri imageUrl = result.getUri();
+                try {
+                    analyseImage(MediaStore.Images.Media.getBitmap(getContentResolver(), imageUrl));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void analyseImage(Bitmap bitmap) {
+        if(bitmap == null) {
+            Toast.makeText(this, "There was an error", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        imageView.setImageBitmap(null);
+        faceDetectionModels.clear();
+
+        Objects.requireNonNull(bottomSheetRecyclerView.getAdapter()).notifyDataSetChanged();
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        showProgress();
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionFaceDetectorOptions options = new FirebaseVisionFaceDetectorOptions.Builder()
+                .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                .build();
+    }
+
+    private void showProgress() {
+        findViewById(R.id.bottom_sheet_button_image).setVisibility(View.GONE);
+        findViewById(R.id.bottom_sheet_button_progress).setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgress() {
+        findViewById(R.id.bottom_sheet_button_image).setVisibility(View.VISIBLE);
+        findViewById(R.id.bottom_sheet_button_progress).setVisibility(View.GONE);
+    }
+
+
 
     @Override
     public void process(@NonNull Frame frame) {
